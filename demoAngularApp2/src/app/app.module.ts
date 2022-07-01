9974e53a79cf391b08471d8ae08d5f6b99c7ddc8
rishika0102 <rishikaa@shriffle.com>
2022-07-01 13:29:07 +0530

    data binding and day 2 passing and using data with eventbinding

--- demoAngularApp2/src/app/app.module.ts
+++ demoAngularApp2/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { SuccessComponent } from './success/Success.component';
--- demoAngularApp2/src/app/server/server.component.ts
+++ demoAngularApp2/src/app/server/server.component.ts
@@ -3,8 +3,28 @@ import {Component } from '@angular/core';
 @Component({
   selector: '[app-server]',
   templateUrl: './server.component.html'
-
 })
 export class ServerComponent {
-
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } , 2000);
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onPassEvent(event: any){
+    console.log(event);
+  }
 }
