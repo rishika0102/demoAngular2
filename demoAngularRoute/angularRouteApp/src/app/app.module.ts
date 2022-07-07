dbd964655e4ed0398e217c90ce9d7fc5f8ab7290
rishika0102 <rishikaa@shriffle.com>
2022-07-07 13:10:42 +0530

    passing and fetching data from routes implementation

--- demoAngularRoute/angularRouteApp/src/app/app.module.ts
+++ demoAngularRoute/angularRouteApp/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { RouterComponent } from './router/router.component';
 import { RouterlinksComponent } from './routerlinks/routerlinks.component';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 
 const appRoutes: Routes = [
   { path: 'route', component: RouterComponent},
-  { path: 'routelink', component: RouterlinksComponent}
+  { path: 'routelink', component: RouterlinksComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'users/:id/:name', component: UserComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     RouterComponent,
-    RouterlinksComponent
+    RouterlinksComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
--- demoAngularRoute/angularRouteApp/src/app/routerlinks/routerlinks.component.ts
+++ demoAngularRoute/angularRouteApp/src/app/routerlinks/routerlinks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-routerlinks',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouterlinksComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  onLoadServers() {
+    this.router.navigate(['/route']);
   }
 
 }
